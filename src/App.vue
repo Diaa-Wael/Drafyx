5f3fd2049e32728dd363e4cd12126c481894db42
Diaa-Wael <diaawael25@gmail.com>
2026-09-16 22:33:39 +0300

    feat: disable language selector in AcApSettingManager

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,6 @@ import { MlCadViewer } from '@mlightcad/cad-viewer'
 import { AcApSettingManager } from '@mlightcad/cad-simple-viewer'
 import { registerLibreDwg } from './registerLibreDwg'
 
-AcApSettingManager.instance.isShowLanguageSelector = false
-
 registerLibreDwg()
 
 AcApSettingManager.instance.isShowToolbar = true
